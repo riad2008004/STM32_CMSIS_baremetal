d8775753fbf8243d64febb77e1938dee1d927c63
riad2008004 <arifulislamriad5@gmail.com>
2025-01-16 04:02:18 +0600

    change main

--- src/main.c
+++ src/main.c
@@ -1,18 +1,22 @@
 #include "stm32f103x6.h"
 
-void delay(volatile uint32_t count) {
-    while (count--) {
+void delay(volatile uint32_t count)
+{
+    while (count--)
+    {
         __asm__("nop");
     }
 }
 
-int main(void) {
+int main(void)
+{
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
     GPIOC->CRH &= ~GPIO_CRH_CNF13;
     GPIOC->CRH |= GPIO_CRH_MODE13_1;
 
-    while (1) {
-        GPIOC->ODR ^= GPIO_ODR_ODR13; // Toggle LEDD
+    while (1)
+    {
+        GPIOC->ODR ^= GPIO_ODR_ODR13; // Toggle LED
         delay(500000);
     }
 }
